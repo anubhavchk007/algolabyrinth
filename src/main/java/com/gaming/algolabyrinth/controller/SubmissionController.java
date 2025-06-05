ef775b6ff053b1a74a0ec27be226e163640fe086
anubhavchk007 <anubhavclasher2262@gmail.com>
2025-06-06 00:48:57 +0530

    fixed post api

--- src/main/java/com/gaming/algolabyrinth/controller/SubmissionController.java
+++ src/main/java/com/gaming/algolabyrinth/controller/SubmissionController.java
@@ -4,10 +4,7 @@ import com.gaming.algolabyrinth.dto.SubmitSolutionRequestDTO;
 import com.gaming.algolabyrinth.dto.SubmitSolutionResponseDTO;
 import com.gaming.algolabyrinth.service.SubmissionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/room")
@@ -15,6 +12,7 @@ public class SubmissionController {
 
     @Autowired private SubmissionService submissionService;
 
+    @PostMapping("/{level}/submit")
     public SubmitSolutionResponseDTO submitSolution(
             @PathVariable int level,
             @RequestBody SubmitSolutionRequestDTO request) throws Exception {
